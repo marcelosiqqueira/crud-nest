9f8dc19430baa04fa5541c930bdd5d2a2d052541
Marcelo Siqueira Filho <marcelosiqqueira@gmail.com>
2023-04-17 22:35:47 -0300

    create user dto and add to route post

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+import { IsString, IsEmail, IsStrongPassword} from "class-validator";
+
+export class CreateUserDto {
+
+    @IsString()
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsStrongPassword({
+        minLength: 6,
+        minLowercase: 0,
+        minNumbers: 0,
+        minSymbols: 0,
+        minUppercase: 0
+    })
+    password: string
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Get, Param, Post, Put, Patch, Delete } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Controller('users')
 export class UserController {
 
     @Post()
-    async create(@Body() body ){
-        return {body};
+    async create(@Body() { email, name, password }: CreateUserDto ){
+        return { email, name, password };
     }
 
     @Get()
